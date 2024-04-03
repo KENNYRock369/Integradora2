194560c671662654b85be228270d240d6a31b6fd
BrayanRey <bryanreytt124@gmail.com>
2024-04-03 16:00:04 -0600

    Hice un cambio

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import "react-native-gesture-handler"
 
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from "./Navigation";
-
+//Este es un cambio
 export default function App() {
   return <Navigation/>;
 }
